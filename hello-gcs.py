b1ba2cc72df357b139049390f2c9d0f8f2fd45d4
ibrezm1 <ibrezm1@gmail.com>
2022-01-26 19:25:56 +0530

    a

--- hello-gcs.py
+++ hello-gcs.py
@@ -1,5 +1,18 @@
 from airflow import DAG
 from airflow.operators import PythonOperator
+
+# Following are defaults which can be overridden later on
+default_args = {
+    'owner': 'manasi',
+    'depends_on_past': False,
+    'start_date': datetime(2016, 4, 15),
+    'email': ['manasidalvi14@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+
 dag = DAG(
     'add_gcp_connection',
     default_args=default_args,
