871e735dbcbca2152a6140d52136155aaa84af61
ibrezm1 <ibrezm1@gmail.com>
2022-01-26 19:31:08 +0530

    a

--- hello-gcs.py
+++ hello-gcs.py
@@ -1,18 +1,55 @@
-from airflow import DAG
-from airflow.operators import PythonOperator
-from datetime import datetime, timedelta
+import json
 
-# Following are defaults which can be overridden later on
+from airflow import DAG, settings
+from airflow.models import Connection
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+
+from common.utils import get_default_google_cloud_connection_id
+
 default_args = {
-    'owner': 'manasi',
+    'owner': 'airflow',
+    'email': ['airflow@example.com'],
     'depends_on_past': False,
-    'start_date': datetime(2016, 4, 15),
-    'email': ['manasidalvi14@gmail.com'],
+    'start_date': datetime(2001, 01, 01),
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=1),
+    'retries': 5,
+    'priority_weight': 1000,
 }
+
+
+def add_gcp_connection(ds, **kwargs):
+    """"Add a airflow connection for GCP"""
+    new_conn = Connection(
+        conn_id=get_default_google_cloud_connection_id(),
+        conn_type='google_cloud_platform',
+    )
+    scopes = [
+        "https://www.googleapis.com/auth/pubsub",
+        "https://www.googleapis.com/auth/datastore",
+        "https://www.googleapis.com/auth/bigquery",
+        "https://www.googleapis.com/auth/devstorage.read_write",
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/cloud-platform",
+    ]
+    conn_extra = {
+        "extra__google_cloud_platform__scope": ",".join(scopes),
+        "extra__google_cloud_platform__project": "your-gcp-project",
+        "extra__google_cloud_platform__key_path": '/var/local/google_cloud_default.json'
+    }
+    conn_extra_json = json.dumps(conn_extra)
+    new_conn.set_extra(conn_extra_json)
+
+    session = settings.Session()
+    if not (session.query(Connection).filter(Connection.conn_id == new_conn.conn_id).first()):
+        session.add(new_conn)
+        session.commit()
+    else:
+        msg = '\n\tA connection with `conn_id`={conn_id} already exists\n'
+        msg = msg.format(conn_id=new_conn.conn_id)
+        print(msg)
+
 
 dag = DAG(
     'add_gcp_connection',
